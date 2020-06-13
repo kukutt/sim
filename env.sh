9f57cc342ba388bc096782ea4ed655861e475305
panyao <panyao@yeah.net>
2020-06-14 06:11:05 +0800

    wujian100仿真通过

--- env.sh
+++ env.sh
@@ -1,2 +1,7 @@
-export PATH=$PWD/run/bin:$PATH
-export LD_LIBRARY_PATH=$PWD/run/lib:$LD_LIBRARY_PATH
+#!/bin/bash
+homePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $homePath  
+
+export PATH=$homePath/run/bin:$PATH
+export LD_LIBRARY_PATH=$homePath/run/lib:$LD_LIBRARY_PATH
+export TOOL_PATH=$homePath/run/toolchain/
