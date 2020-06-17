e128e26dc46ba195e1a1e413fa44c465c3a69aa0
panyao <panyao@yeah.net>
2020-06-17 23:37:59 +0800

    输出各种文件格式说明

--- wujian100/test01/crt0.s
+++ wujian100/test01/crt0.s
@@ -1,34 +1,17 @@
-/*
-Copyright (c) 2019 Alibaba Group Holding Limited
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-*/
 .text
 
 .global __start
 __start:
-  li  x13,0x2001fff0
-  li  x14,0x38
-  sw  x14,0(x13)
-  sw  x14,0(x13)
-  sw  x14,0(x13)
-  sw  x14,0(x13)
-  sw  x14,0(x13)
-  sw  x14,0(x13)
-  sw  x14,0(x13)
-  sw  x14,0(x13)
-  li  x14,0x0a
-  sw  x14,0(x13)
-  li  x14,0x0d
-  sw  x14,0(x13)
-  li  x13, 0x20007c50 
-  li  x14, 0x2002
-  sw  x14, 0(x13)
-  sw  x14, 0(x13)
-  sw  x14, 0(x13)
-
+  li  x11,0x2001fff0
+  li  x10,0x38  #打印8
+  sw  x10,0(x11)
+  li  x10,0x0a  #换行
+  sw  x10,0(x11)
+  li  x10,0x0d  #回车
+  sw  x10,0(x11)
+__simEnd:
+  li  x11, 0x20007c50 
+  li  x10, 0x2002 #sim结束
+  sw  x10, 0(x11)
+  jal __simEnd
+  
